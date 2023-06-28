ba1046bd8ae6cd250724c900e7a55f2e72ac9dea
felipeslopes <felipesl.lopes@hotmail.com>
2023-06-28 14:40:58 -0300

    selecionando movimentações por data

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,10 @@
+import Ionicons from '@expo/vector-icons/Ionicons';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { useIsFocused } from '@react-navigation/native';
 import { format, isPast } from 'date-fns';
 import React, { useContext, useEffect, useState } from 'react';
 import { Alert, FlatList, StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Header from '../../components/Header';
 import ListHistoric from '../../components/ListHistoric';
@@ -11,19 +15,24 @@ export default function Home() {
 
     const [balance, setBalance] = useState(0);
     const [movies, setMovies] = useState([]);
+    const [array, setArray] = useState([]);
+    const [show, setShow] = useState(false);
+    const [newDate, setNewDate] = useState(new Date());
+    const focused = useIsFocused();
 
     const { user } = useContext(AuthContext);
     const uid = user && user.uid;
 
     useEffect(() => {
         (async () => {
-            await firebase.database().ref("usuario").child(uid).on('value', (snapshot) => {
+            firebase.database().ref("usuario").child(uid).on('value', (snapshot) => {
                 setBalance(snapshot.val().saldo)
             })
 
             await firebase.database().ref('historico').child(uid).orderByChild('date')
-                .equalTo(format(new Date(), 'dd/MM/yy'))
-                .limitToLast(10).on('value', (snapshot) => {
+                .equalTo(format(newDate, 'dd/MM/yy'))
+                .limitToLast(10).once('value')
+                .then((snapshot) => {
                     setMovies([]);
 
                     snapshot.forEach((childItem) => {
@@ -33,12 +42,15 @@ export default function Home() {
                             valor: childItem.val().valor,
                             date: childItem.val().date,
                         }
-
-                        setMovies(oldArray => [...oldArray, list].reverse());
+                        setMovies(oldArray => [...oldArray, list]);
                     });
+                    setArray(movies.slice().reverse());
+                })
+                .then(() => {
                 })
         })();
-    }, [])
+        console.log(newDate)
+    }, [focused, newDate])
 
     function handleDelete(data) {
 
@@ -52,14 +64,8 @@ export default function Home() {
             'Atenção!',
             `${data.tipo === 'receita' ? 'Receita' : 'Despesa'} R$${data.valor} - ${data.date}`,
             [
-                {
-                    text: 'Cancelar',
-                    style: 'cancel'
-                },
-                {
-                    text: 'Excluir',
-                    onPress: () => handleDeleteSucess(data)
-                }
+                { text: 'Cancelar', style: 'cancel' },
+                { text: 'Excluir', onPress: () => handleDeleteSucess(data) }
             ]
         )
     }
@@ -76,6 +82,17 @@ export default function Home() {
             .catch((error) => { console.log(error) })
     }
 
+    function handleDate() {
+        setShow(true);
+    }
+
+    function closeHandleDate(event, date) {
+        setShow(false);
+        if (date) {
+            setNewDate(date);
+        }
+    }
+
     return (
         <SafeAreaView style={styles.container}>
 
@@ -84,18 +101,33 @@ export default function Home() {
             <Text style={styles.name}>{user && user.name}</Text>
             <Text style={styles.balance}>R${balance.toFixed(2)}</Text>
 
-            <Text style={styles.latestMovies}>Últimas movimentações</Text>
-
+            <View style={{ flexDirection: 'row', marginBottom: 5, }}>
+                <Text style={styles.latestMovies}>Últimas movimentações</Text>
+                <TouchableOpacity onPress={handleDate} activeOpacity={0.7}>
+                    <Ionicons name='calendar-outline' size={25} color={'white'} />
+                </TouchableOpacity>
+            </View>
 
             <View style={styles.box}>
-                <FlatList
-                    style={{ margin: 10 }}
-                    data={movies}
-                    keyExtractor={item => item.key}
-                    renderItem={({ item }) => (<ListHistoric data={item} deleteItem={handleDelete} />)}
-                    showsVerticalScrollIndicator={false}
-                />
+                {array.length == 0 ? <Text style={styles.notBalance}>Você não tem nenhum registro nessa data!</Text> :
+                    <FlatList
+                        style={{ margin: 10 }}
+                        data={array}
+                        keyExtractor={item => item.key}
+                        renderItem={({ item }) => (<ListHistoric data={item} deleteItem={handleDelete} />)}
+                        showsVerticalScrollIndicator={false}
+                    />
+                }
             </View>
+
+            {show &&
+                <DateTimePicker
+                    value={newDate}
+                    mode='date'
+                    display='default'
+                    onChange={closeHandleDate}
+                />
+            }
 
         </SafeAreaView>
     )
@@ -119,11 +151,18 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         marginBottom: 40,
     },
+    notBalance: {
+        marginTop: 100,
+        textAlign: 'center',
+        fontSize: 17,
+        fontStyle: 'italic',
+    },
     latestMovies: {
         fontSize: 17,
         color: '#00FF00',
         marginLeft: 10,
         marginBottom: 4,
+        marginRight: 10,
     },
     box: {
         flex: 1,
